5f3e7cd20043a27f7dece57961de58d305336c0b
TheR16H <rashawn5hall@gmail.com>
2024-09-11 14:59:04 -0400

    i think i added the nessacary work so the thing deploys. i worked in server.js for this session

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection'); // This should use the connection from the first file
@@ -6,6 +9,11 @@ const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,3 +30,17 @@ db.once('open', () => {
 
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
