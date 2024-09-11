995f8b2892512bd4758e647b5710e437e23b8257
TheR16H <rashawn5hall@gmail.com>
2024-09-11 14:04:43 -0400

    type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,44 +2,41 @@ const { gql } = require("apollo-server-express");
  
 const typeDefs = gql 
 type User {
-    id: ID
-    username: String
-    email: String
-    bookCount: INTEGER
-    savedBooks: [book] //array of "book" type
+    id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book] //array of "book" type
 }
-`Book` type:
+type Book {
+    bookId: ID // (Not the `_id`, but the book's `id` value returned from Google's Book API.)
+    authors: [String] // (An array of strings, as there may be more than one author.)
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+}
+ type Auth {
+    token: ID!
+    user: User
+  }
+type Mutation {
 
-      * `bookId` (Not the `_id`, but the book's `id` value returned from Google's Book API.)
+    login(email: String!, password: String!): Auth // Accepts an email and password as parameters; returns an `Auth` type.
+    addUser(username: String!, email: String!, password: String!): Auth //Accepts a username, email, and password as parameters; returns an `Auth` type.
+    saveBook(
+    authors: [String]! 
+    description: String!
+    title: String!
+    bookId: ID!
+    image: String!
+ )  //Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a `User` type. (Look into creating what's known as an `input` type to handle all of these parameters!)
+    removeBook(bookId: ID!): User // Accepts a book's `bookId` as a parameter; returns a `User` type.
 
-      * `authors` (An array of strings, as there may be more than one author.)
-
-      * `description`
-
-      * `title`
-
-      * `image`
-
-      * `link`
-`Query` type:
-
-      * `me`: Which returns a `User` type.
-  
-    * `Mutation` type:
-
-      * `login`: Accepts an email and password as parameters; returns an `Auth` type.
-
-      * `addUser`: Accepts a username, email, and password as parameters; returns an `Auth` type.
-
-      * `saveBook`: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a `User` type. (Look into creating what's known as an `input` type to handle all of these parameters!)
-
-      * `removeBook`: Accepts a book's `bookId` as a parameter; returns a `User` type.
-
-
-    * `Auth` type:
-
-      * `token`
-
-      * `user` (References the `User` type.)
-
+}
+    type Query {
+    me: User
+  }
+ 
+ ;
 module.exports = typeDefs;
