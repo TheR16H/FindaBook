5421a3108e52d2be4d622b1f9aaf935745d9c26d
TheR16H <rashawn5hall@gmail.com>
2024-09-11 13:39:01 -0400

    forgot

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
-Define the query and mutation functionality to work with the Mongoose models.
+// Define the query and mutation functionality to work with the Mongoose models.
+// server/models --- gonna require this
 
 module.exports = resolvers;
