2765caf995209b3477d77d504656805c8ac7a998
TheR16H <rashawn5hall@gmail.com>
2024-09-11 14:07:50 -0400

    i think i finished typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,6 @@
 const { gql } = require("apollo-server-express");
  
-const typeDefs = gql 
+const typeDefs = gql`
 type User {
     id: ID!
     username: String!
@@ -9,8 +9,8 @@ type User {
     savedBooks: [Book] //array of "book" type
 }
 type Book {
-    bookId: ID // (Not the `_id`, but the book's `id` value returned from Google's Book API.)
-    authors: [String] // (An array of strings, as there may be more than one author.)
+    bookId: ID 
+    authors: [String] 
     description: String!
     title: String!
     image: String!
@@ -22,21 +22,21 @@ type Book {
   }
 type Mutation {
 
-    login(email: String!, password: String!): Auth // Accepts an email and password as parameters; returns an `Auth` type.
-    addUser(username: String!, email: String!, password: String!): Auth //Accepts a username, email, and password as parameters; returns an `Auth` type.
+    login(email: String!, password: String!): Auth 
+    addUser(username: String!, email: String!, password: String!): Auth 
     saveBook(
     authors: [String]! 
     description: String!
     title: String!
     bookId: ID!
     image: String!
- )  //Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a `User` type. (Look into creating what's known as an `input` type to handle all of these parameters!)
-    removeBook(bookId: ID!): User // Accepts a book's `bookId` as a parameter; returns a `User` type.
+ )  
+    removeBook(bookId: ID!): User 
 
 }
     type Query {
     me: User
   }
  
- ;
+ `;
 module.exports = typeDefs;
