cfadf8d76ca8f3ed3686fe31dd731c984c3feebe
TheR16H <rashawn5hall@gmail.com>
2024-09-12 21:56:09 -0400

    syntax error in app really

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-mport './App.css';
+import './App.css';
 import { Outlet, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
